a5133ae5b771a59ed4303ef9e9d556d220036832
road037019 <road037019@gmail.com>
2018-04-13 10:32:09 +0800

    提交台阶跳问题

---
+++ 使用排列组合进行台阶跳.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jumpFloorII(int number) {
+        int count = 0;
+        if(number <= 0)
+            return 0;
+        else if(number == 1)
+            return 1;
+        else
+            for(int i = 1; i < number; i++)
+                count += jumpFloorII(i); //总的跳的次数肯定是第一步跳1阶 + 第一次跳2阶 + 第一次跳3阶 + ····+ 第一次跳n-1阶梯 +
+        return count + 1; //一次全跳完；
+    }
+};
